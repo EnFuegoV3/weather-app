60d47e3715f4ab1f787cdabe9039508e615e01d1
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-05-08 21:40:29 -1000

    added marine report to create surf report, both APIs working need to add html/dom elements

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
+const main = document.querySelector('main');
+const header = document.querySelector('h1');
+
 async function getWeather(location) {
     try {
     let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=92f555c514464e9ba1b20137230805&q=${location}`, {mode: "cors"})
     let newData = await response.json();
-       
-    return newData;
+    // console.log(response)
+    console.log(newData)
+    // console.log(newData.current.condition.text)
+    if(newData.error) {
+        alert(newData.error.message);
+    } else {
+        header.textContent = ""
+        // const condition = document.createElement('div');
+        // condition.textContent = newData.current.condition.text;
+        // main.appendChild(condition);
+    }
 
     } catch (error) {alert(error)}
 };
 
+async function surfReport(location) {
+    try {
+    let response = await fetch(`https://api.weatherapi.com/v1/marine.json?key=92f555c514464e9ba1b20137230805&q=${location}`, {mode: "cors"})
+    let newReport = await response.json();
+    // console.log(response)
+    console.log(newReport)
+    // console.log(newData.current.condition.text)
+    if(newData.error) {
+        alert(newReport.error.message);
+    } else {
+        header.textContent = ""
+        // const condition = document.createElement('div');
+        // condition.textContent = newData.current.condition.text;
+        // main.appendChild(condition);
+    }
+
+    } catch (error) {alert(error)}
+};
+
+const cityName = document.querySelector('#city-name');
+const submit = document.querySelector('#submit');
+
+// let weatherData
+
+submit.addEventListener('click', () => {
+    getWeather(cityName.value);
+    surfReport(cityName.value);
+    
+})
 
 
 
-console.log(getWeather("honolulu"))
+
+
