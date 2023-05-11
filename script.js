46e2f15b14a10554d8769cac87bed607729c025e
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-05-10 20:36:22 -1000

    html layout draft done, need to do JS to append api data

--- script.js
+++ script.js
@@ -7,39 +7,32 @@ async function getWeather(location) {
     try {
     let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=92f555c514464e9ba1b20137230805&q=${location}`, {mode: "cors"});
     let newData = await response.json();
-    // console.log(response)
     console.log(newData)
-    // console.log(newData.current.condition.text)
     if(newData.error) {
         alert(newData.error.message);
     } else {
         header.textContent = newData.location.name + ", " + newData.location.region;
-        // const condition = document.createElement('div');
-        // condition.textContent = newData.current.condition.text;
-        // main.appendChild(condition);
+       
     }
 
     } catch (error) {console.log(error)}
 };
 
-async function surfReport(location) {
-    try {
-    let response = await fetch(`https://api.weatherapi.com/v1/marine.json?key=92f555c514464e9ba1b20137230805&q=${location}`, {mode: "cors"})
-    let newReport = await response.json();
-    // console.log(response)
-    console.log(newReport)
-    // console.log(newData.current.condition.text)
-    if(newData.error) {
-        alert(newReport.error.message);
-    } else {
-        // header.textContent = ""
-        // const condition = document.createElement('div');
-        // condition.textContent = newData.current.condition.text;
-        // main.appendChild(condition);
-    }
+// async function surfReport(location) {
+//     try {
+//     let response = await fetch(`https://api.weatherapi.com/v1/marine.json?key=92f555c514464e9ba1b20137230805&q=${location}`, {mode: "cors"})
+//     let newReport = await response.json();
+//     // console.log(response)
+//     console.log(newReport)
+//     // console.log(newData.current.condition.text)
+//     if(newData.error) {
+//         alert(newReport.error.message);
+//     } else {
+        
+//     }
 
-    } catch (error) {alert(error)}
-};
+//     } catch (error) {alert(error)}
+// };
 
 
 
@@ -48,7 +41,7 @@ async function surfReport(location) {
 submit.addEventListener('click', (e) => {
     e.preventDefault();
     getWeather(cityName.value);
-    surfReport(cityName.value)
+    // surfReport(cityName.value)
 });
 
 // getWeather("honolulu")
